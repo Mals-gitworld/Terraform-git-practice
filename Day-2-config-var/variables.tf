512a2ed8dd8ce00939bcbdd137f3cd5694865e72
Malathy <malathy_murugan@yahoo.co.in>
2026-03-09 21:05:33 +0530

    day2

---
+++ Day-2-config-var/variables.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+  description = "ami value ami-02dfbd4ff395f2a1b"
+  default     = ""
+  type        = string
+}
+
+variable "instance_type" {
+  description = "pass instance tyoe t2.medium"
+  default     = ""
+  type        = string
+}
